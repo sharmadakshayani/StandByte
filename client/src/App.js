602ffb1a6d4a90fee75fa194cabab8514566a724
Dakshayani Sharma <sharmadakshayani1211@gmail.com>
2026-02-28 09:54:03 +0530

    Initial React Setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1>StandByte</h1>
+      <p>Adaptive Screen-Time Interruption Platform</p>
+    </div>
+  );
+}
+
+export default App; 
